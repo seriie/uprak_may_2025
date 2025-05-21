6906ea5ea1c840c857afbdfe557c63415e46237c
serrie <mohammadzidane058@gmail.com>
2025-05-21 11:17:08 +0700

    feat: apply theme

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useTheme } from "../context/ThemeContext";
 import { RxCross2 } from "react-icons/rx";
 import { motion } from "framer-motion";
 import homeIcon from "../assets/icon/navbar/home.png";
@@ -15,6 +16,7 @@ import youtubeIcon from "../assets/icon/footer/youtube.png";
 
 export default function Sidebar({ isOpen, setIsOpen }) {
   const [btmOpened, setBtmOpened] = useState(false);
+  const { theme } = useTheme();
 
   const sidebarIcon = [
     { id: 1, name: "Home", icon: homeIcon },
@@ -76,51 +78,51 @@ export default function Sidebar({ isOpen, setIsOpen }) {
         initial={{ x: "-100%" }}
         animate={{ x: isOpen ? 0 : "-100%" }}
         transition={{ duration: 0.3, ease: "easeOut" }}
-        className={`fixed top-0 left-0 z-20 h-full w-64 bg-slate-700 p-4`}
+        className={`fixed top-0 left-0 z-20 h-full w-64 ${theme === "dark" ? 'bg-slate-700' : 'bg-slate-100'} p-4`}
       >
         <div className="flex justify-between">
-          <p className="font-bold text-xl text-slate-100">Menu</p>
+          <p className={`font-bold text-xl ${theme === "dark" ? 'text-slate-100' : 'text-slate-800'}`}>Menu</p>
           <RxCross2
-            className="text-slate-100 text-3xl cursor-pointer"
+            className={`${theme === "dark" ? 'text-slate-100' : 'text-slate-800'} text-3xl cursor-pointer`}
             onClick={() => setIsOpen(false)}
           />
         </div>
-        <div className="flex flex-col mt-4 border-b pb-6 border-slate-600">
+        <div className={`flex flex-col mt-4 border-b pb-6 ${theme === "dark" ? 'border-slate-600' : 'border-slate-400'}`}>
           {sidebarIcon.map((item) => (
             <button
               key={item.id}
-              className="flex items-center gap-2 p-2 cursor-pointer hover:bg-slate-600 rounded-md"
+              className={`flex items-center gap-2 p-2 cursor-pointer ${theme === "dark" ? 'hover:bg-slate-600' : 'hover:bg-slate-300'} rounded-md`}
             >
-              <img src={item.icon} className="w-6 h-6" />
-              <p className="font-bold text-slate-100">{item.name}</p>
+              <img src={item.icon} className={`w-5 h-5 ${theme === "dark" ? '': 'brightness-[.1]'}`} />
+              <p className={`font-bold ${theme === "dark" ? 'text-slate-100' : 'text-slate-800'}`}>{item.name}</p>
             </button>
           ))}
         </div>
         <div className="mt-4">
           <button
             onClick={() => setBtmOpened(!btmOpened)}
-            className="w-full p-2 text-start font-medium text-slate-100 cursor-pointer hover:bg-slate-600 rounded-md"
+            className={`w-full p-2 text-start font-medium ${theme === "dark" ? 'text-slate-100 hover:bg-slate-600' : 'text-slate-800 hover:bg-slate-300'} cursor-pointer rounded-md`}
           >
-            {btmOpened ? "Less" : "More"}...
+            {btmOpened ? "Less" : "More"}
           </button>
           {btmOpened &&
             <div>
                 <div className="flex flex-col mt-4">
                     {linkItem.map((item, idx) => (
                         <a key={idx} href={item.link}>
-                            <button className="p-2 w-full text-start cursor-pointer hover:bg-slate-600 rounded-md text-slate-100 font-medium">{item.name}</button>
+                            <button className={`p-2 w-full text-start cursor-pointer ${theme === "dark" ? 'hover:bg-slate-600 text-slate-100' : 'hover:bg-slate-300 text-slate-800'} rounded-md font-medium`}>{item.name}</button>
                         </a>
                     ))}
                 </div>
                 <div className="mt-4">
                     <div className="flex gap-2">
                         {socialMediaIcon.map((item, idx) => (
-                            <a key={idx} href={item.link} className={`${item.background} p-2 rounded-full`}>
-                                <img src={item.icon} className="w-6 h-6" alt={item.name} />
+                            <a key={idx} href={item.link} className={``}>
+                                <img src={item.icon} className={`w-10 h-10 ${item.background} p-2 rounded-full ${theme === "dark" ? '' : 'brightness-[.2] hover:brightness-100'}`} alt={item.name} />
                             </a>
                         ))}
                     </div>
-                    <p className="mt-2 text-xs text-slate-100">Copyright &copy; {new Date().getFullYear()} <a className="font-bold cursor-pointer hover:text-sky-500" href="/">Petani Kode</a></p>
+                    <p className={`mt-2 text-xs ${theme === "dark" ? 'text-slate-100' : 'text-slate-800'}`}>Copyright &copy; {new Date().getFullYear()} <a className="font-bold cursor-pointer hover:text-sky-500" href="/">Petani Kode</a></p>
                 </div>
             </div>
             }
