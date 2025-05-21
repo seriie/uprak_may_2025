cbc802a5e0bdc32769c3e1bbf3696f7aa0456e41
serrie <mohammadzidane058@gmail.com>
2025-05-21 11:20:11 +0700

    feat: update display

--- src/components/content/Category-card.jsx
+++ src/components/content/Category-card.jsx
@@ -5,7 +5,7 @@ export default function CategoryCard({ item }) {
   
   return (
     <div
-      className={`rounded-xl cursor-pointer w-full md:w-80 ${theme === "dark" ? 'bg-slate-800/30 border-slate-700' : 'bg-slate-200/30 border-slate-300'} backdrop-blur-lg p-6 flex flex-col gap-3 border hover:scale-[1.03] hover:border-teal-400 transition-all duration-300 shadow-lg`}
+      className={`rounded-xl cursor-pointer w-full md:w-92 ${theme === "dark" ? 'bg-slate-800/30 border-slate-700' : 'bg-slate-200/30 border-slate-300'} backdrop-blur-lg p-6 flex flex-col gap-3 border hover:scale-[1.03] hover:border-teal-400 transition-all duration-300 shadow-lg`}
     >
       <div
         className={`p-3 w-12 h-12 rounded-xl ${item.iconColor} flex items-center justify-center shadow-md`}
--- src/components/content/Category.jsx
+++ src/components/content/Category.jsx
@@ -4,7 +4,7 @@ import listIcon from '../../assets/icon/content/list.png';
 import CategoryCard from './Category-card';
 import Heading from './Heading';
 
-export default function Category() {
+export default function     Category() {
     const cardItems = [
         { id: 1, icon: codeIcon, iconColor: "bg-indigo-500", title: "Basic Programming", description: "Baca tutorial dasar-dasar pemrograman menggunakan C, C++, C#, Java, Javascript, dan masih banyak lagi." },
         { id: 2, icon: ethernetIcon, iconColor: "bg-orange-500", title: "Web Programming", description: "Baca tutorial cara membuat web. Mulai dari HTML, CSS, JS, PHP, MySQL, Codeigniter, React, dan masih banyak lagi." },
@@ -14,7 +14,7 @@ export default function Category() {
     return (
         <>
             <Heading subTitle="KATEGORI" title="Mau belajar apa hari ini?" desc="Temukan tutorial berdasarkan minatmu." />
-            <div className='flex flex-wrap w-full justify-center mt-8 gap-20'>
+            <div className='flex flex-wrap w-full justify-center mt-8 gap-10'>
                 {cardItems.map((item) => (
                     <CategoryCard key={item.id} item={item} />
                 ))}
