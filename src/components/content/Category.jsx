5bad0504fe1e2c652132b8f0eba7ac99282aefba
serrie <mohammadzidane058@gmail.com>
2025-05-21 21:51:43 +0700

    fix: display

--- src/components/content/Category.jsx
+++ src/components/content/Category.jsx
@@ -14,7 +14,7 @@ export default function     Category() {
     return (
         <>
             <Heading subTitle="KATEGORI" title="Mau belajar apa hari ini?" desc="Temukan tutorial berdasarkan minatmu." />
-            <div className='flex flex-wrap w-full justify-center mt-8 gap-10'>
+            <div className='flex flex-wrap w-full justify-center mt-8 gap-5'>
                 {cardItems.map((item) => (
                     <CategoryCard key={item.id} item={item} />
                 ))}
